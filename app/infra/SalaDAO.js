4aed4fee355f746ce1301f49538f01b33a7e5e4c
Nathalia Pissuti <nathi.pg2@gmail.com>
2018-06-29 19:16:49 -0300

    DAO

---
+++ app/infra/SalaDAO.js
@@ -0,0 +1,45 @@
+function SalaDAO( connection ) {
+	this._connection = connection;
+}
+
+SalaDAO.prototype.listar = callback => {
+	let sql = "SELECT * FROM Sala";
+	conn.query( sql, [], callback );
+}
+
+SalaDAO.prototype.detalhes = ( id, callback ) => {
+	let sql = "SELECT * FROM Sala WHERE idSala = ?";
+	let values = [ id ];
+	conn.query( sql, values, callback );
+}
+
+SalaDAO.prototype.insert = ( dados, callback ) => {
+	let sql = `
+		INSERT INTO Sala
+			( tipoSala, status, descricao, local, estadoConservacao, numero )
+		VALUES
+			( ?, ?, ?, ?, ?, ? )`;
+	let values = [ dados.tipoSala, dados.status, dados.descricao, dados.local, dados.estadoConservacao, dados.numero ];
+	conn.query( sql, values, callback );
+}
+
+SalaDAO.prototype.update = ( dados, callback ) => {
+	let sql = `
+		UPDATE Sala
+		SET
+			tipoSala = ?,
+			status = ?,
+			descricao = ?,
+			local = ?,
+			estadoConservacao = ?,
+			numero = ?
+		WHERE idSala = ?`;
+	let values = [ dados.tipoSala, dados.status, dados.descricao, dados.local, dados.estadoConservacao, dados.numero, dados.idSala ];
+	conn.query( sql, values, callback );
+}
+
+SalaDAO.prototype.delete = ( id, callback ) => {
+	let sql = "DELETE FROM Sala WHERE idSala = ?";
+	let values = [ id ];
+	conn.query( sql, values, callback );
+}
